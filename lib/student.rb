416080c946f15b5878a702978fa15144cfeb8b98
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-14 09:38:46 -0700

    Add initialize, attr_reader, #log_score, and #grade

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
 class Student
+  attr_reader :name, :age, :scores
+
+  def initialize(data)
+    @name = data[:name]
+    @age = data[:age]
+    @scores = []
+  end
+
+  def log_score(score)
+    @scores << score
+  end
+
+  def grade
+    scores.sum(0.0) / scores.size
+  end
+
 
 end
