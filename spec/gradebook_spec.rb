55dc16d146f8967b1c71a342bf9a4c9d101ba492
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-14 15:14:51 -0700

    Add TDD for exists, instructor, and courses

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -12,8 +12,22 @@ RSpec.describe Gradebook do
     expect(@gradebook).to be_a(Gradebook)
   end
 
-  it "#list_all_students" do
-    expect(@gradebook.list_all_students).to eq([])
+  it "has an instructor" do
+    expect(@gradebook.instructor).to eq("Kat")
   end
+
+  describe "courses" do
+    it "#list_all_students" do
+    expect(@gradebook.courses).to eq([])
+
+    calc = Course.new("Calculus", 2)
+    trig = Course.new("Trigonometery", 3)
+
+    expect(@gradebook.add_course(calc)).to eq([calc])
+    expect(@gradebook.add_course(trig)).to eq([calc, trig])
+
+    expect(@gradebook.courses).to eq([calc, trig])
+  end
+end
 
 end
