7ceb0c9e60d2a788553266213c0f71c694664e2b
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-14 09:52:13 -0700

    Remove _ after student

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -3,22 +3,22 @@ require './lib/student'
 
 RSpec.describe Student do
   before(:each) do
-    @student_1 = Student.new({name: "Morgan", age: 21})
+    @student1 = Student.new({name: "Morgan", age: 21})
   end
 
   it "exists with attributes" do
-    expect(@student_1).to be_instance_of(Student)
-    expect(@student_1.name).to eq("Morgan")
-    expect(@student_1.age).to eq(21)
+    expect(@student1).to be_instance_of(Student)
+    expect(@student1.name).to eq("Morgan")
+    expect(@student1.age).to eq(21)
   end
 
   it "logs scores to form a grade" do
-    expect(@student_1.scores).to eq([])
+    expect(@student1.scores).to eq([])
 
-    @student_1.log_score(89)
-    @student_1.log_score(78)
+    @student1.log_score(89)
+    @student1.log_score(78)
 
-    expect(@student_1.scores).to eq([89, 78])
-    expect(@student_1.grade).to eq(83.5)
+    expect(@student1.scores).to eq([89, 78])
+    expect(@student1.grade).to eq(83.5)
   end
 end
