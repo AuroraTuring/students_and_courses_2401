d724e9ce96fb9fea394721d4761e8b7b7f95945f
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-14 09:42:33 -0700

    Change student to student_1

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -3,23 +3,22 @@ require './lib/student'
 
 RSpec.describe Student do
   before(:each) do
-    @student = Student.new({name: "Morgan", age: 21})
+    @student_1 = Student.new({name: "Morgan", age: 21})
   end
 
   it "exists with attributes" do
-    expect(@student).to be_instance_of(Student)
-    expect(@student.name).to eq("Morgan")
-    expect(@student.age).to eq(21)
+    expect(@student_1).to be_instance_of(Student)
+    expect(@student_1.name).to eq("Morgan")
+    expect(@student_1.age).to eq(21)
   end
 
   it "logs scores to form a grade" do
-    expect(@student.scores).to eq([])
+    expect(@student_1.scores).to eq([])
 
-    @student.log_score(89)
-    @student.log_score(78)
+    @student_1.log_score(89)
+    @student_1.log_score(78)
 
-    expect(@student.scores).to eq([89, 78])
-    expect(@student.grade).to eq(83.5)
-    # Average of all the scores
+    expect(@student_1.scores).to eq([89, 78])
+    expect(@student_1.grade).to eq(83.5)
   end
 end
