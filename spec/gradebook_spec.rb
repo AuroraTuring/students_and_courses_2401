cde43ec6b6f2cda6567897ec18eeaa68e3bf63eb
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-14 11:52:56 -0700

    Add before each and it exists

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -4,7 +4,12 @@ require './lib/student'
 require './lib/course'
 
 RSpec.describe Gradebook do
+  before(:each) do
+    @gradebook = Gradebook.new
+  end
 
-
+  it "exists with attributes" do
+    expect(@gradebook).to be_a(Gradebook)
+  end
 
 end
