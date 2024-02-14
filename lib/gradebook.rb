55504f6d77c32e0060578475d79b04e3ea30e108
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-14 11:52:12 -0700

    Add #add_course and other method outlines

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,8 +1,21 @@
 class Gradebook
-
+  attr_reader :instructor, :courses
 
   def initialize(instructor)
     @instructor = instructor
+    @courses = []
   end
+
+  def add_course(corse)
+    @courses << corse
+  end
+
+  def list_all_students
+
+  end
+
+  # def students_below(threshold)
+
+  # end
 
 end
