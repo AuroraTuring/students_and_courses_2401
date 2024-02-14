e6dddd224ce2f0ff60835562e0005b41c5135ec0
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-14 09:51:34 -0700

    Add TDD for if a course is full

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -5,6 +5,8 @@ require './lib/student'
 RSpec.describe Course do
   before(:each) do
     @course = Course.new("Calculus", 2)
+    @student1 = Student.new({name: "Morgan", age:21})
+    @student2 = Student.new({name: "Jordan", age: 29})
   end
 
   it "exists with attributes" do
@@ -14,42 +16,14 @@ RSpec.describe Course do
     expect(@course.students).to eq([])
   end
 
+  it "checks if a course is full" do
+    expect(@course.full).to be false
+
+    @course.enroll(@student1)
+    @course.enroll(@student2)
+
+    expect(@course.students).to eq(@student1, @student2)
+    expect(@course.full).to be true
+  end
 
 end
-
-
-
-
-
-
-
-pry(main)>
-# => #<Course:0x00007fa0a69be328...>
-
-pry(main)> course.name
-# => "Calculus"
-
-pry(main)> course.capacity
-# => 2
-
-pry(main)> course.students
-# => []
-
-pry(main)> course.full?
-# => false
-
-pry(main)> student1 = Student.new({name: "Morgan", age: 21})
-# => #<Student:0x00007fa0a80ae588...>
-
-pry(main)> student2 = Student.new({name: "Jordan", age: 29})
-# => #<Student:0x00007fa0a814f4d8...>
-
-pry(main)> course.enroll(student1)
-
-pry(main)> course.enroll(student2)
-
-pry(main)> course.students
-# => [#<Student:0x00007fa0a80ae588...>, #<Student:0x00007fa0a814f4d8...>]
-
-pry(main)> course.full?
-# => true
