f241751d8de3ec9356a80cfa1419f701ff938f65
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-14 14:43:27 -0700

    #list_all_students

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -5,11 +5,15 @@ require './lib/course'
 
 RSpec.describe Gradebook do
   before(:each) do
-    @gradebook = Gradebook.new
+    @gradebook = Gradebook.new("Kat")
   end
 
   it "exists with attributes" do
     expect(@gradebook).to be_a(Gradebook)
+  end
+
+  it "#list_all_students" do
+    expect(@gradebook.list_all_students).to eq([])
   end
 
 end
