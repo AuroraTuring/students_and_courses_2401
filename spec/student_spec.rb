8a5d8ed842f0bce2d9085e8ece27f10806e9d4ec
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-14 09:39:18 -0700

    Add and pass TDD for student class

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,10 +2,24 @@ require 'rspec'
 require './lib/student'
 
 RSpec.describe Student do
-  it "exists" do
-    morgan = Student.new({name: "Morgan", age: 21})
-    expect(morgan).to be_instance_of(Student)
+  before(:each) do
+    @student = Student.new({name: "Morgan", age: 21})
   end
 
-  
+  it "exists with attributes" do
+    expect(@student).to be_instance_of(Student)
+    expect(@student.name).to eq("Morgan")
+    expect(@student.age).to eq(21)
+  end
+
+  it "logs scores to form a grade" do
+    expect(@student.scores).to eq([])
+
+    @student.log_score(89)
+    @student.log_score(78)
+
+    expect(@student.scores).to eq([89, 78])
+    expect(@student.grade).to eq(83.5)
+    # Average of all the scores
+  end
 end
